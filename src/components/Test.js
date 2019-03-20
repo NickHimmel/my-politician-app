e052848f0c13612e9ef062c428177ce89f581fcf
NickHimmel <himmel.nick@gmail.com>
2019-03-20 17:18:19 -0400

    Add axios and test file

---
+++ src/components/Test.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <h1>This is only a test</h1>
+      </div>
+    );
+  }
+}
+
+export default Test;
