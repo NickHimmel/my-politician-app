31ce01e9d0a4f1972d0857e3b8513af92e21682d
NickHimmel <himmel.nick@gmail.com>
2019-04-05 17:25:44 -0400

    Add Loading component

--- src/components/Politician.js
+++ src/components/Politician.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchPolition } from '../actions/actions.js';
+import Loading from './Loading.js'
 
 class Politician extends Component {
   constructor(props) {
