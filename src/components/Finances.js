78f64095786858df09731ed2ce20c42eb660098c
NickHimmel <himmel.nick@gmail.com>
2019-04-26 12:42:11 -0400

    Add finances component

---
+++ src/components/Finances.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Finances = (props) => {
+  return (
+    <div className="finances politician-info">
+      Summary
+      Top Contributors
+      Top Industries
+      Top Sectors
+      ???????Source of Funds (Campaign Committee)????
+    </div>
+  )
+}
+
+export default Finances
--- src/components/NavForPolitician.js
+++ src/components/NavForPolitician.js
@@ -7,6 +7,7 @@ const NavForPolitician = (props) => {
         <li><button onClick={(e) => props.onClick(e, "terms")}>Terms</button></li>
         <li><button onClick={(e) => props.onClick(e, "votes")}>Last 20 Votes</button></li>
         <li><button onClick={(e) => props.onClick(e, "bills")}>Last 20 Bills Sponsored</button></li>
+        <li><button onClick={(e) => props.onClick(e, "finances")}>Finances</button></li>
       </ul>
     </div>
   );
