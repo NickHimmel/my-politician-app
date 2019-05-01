1e5d86215e40b8d4fa7a0cc51013599bf203ba57
NickHimmel <himmel.nick@gmail.com>
2019-05-01 16:55:30 -0400

    Add social media icons

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Name = (props) => {
+  console.log(props)
   return (
     <div>
       <h3>{props.firstName} {props.lastName} ({props.party})</h3>
-      <p>Next Election: {props.nextElection}</p>
     </div>
   );
 }
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
 const Social = (props) => {
+  const homepage = (url) => {
+    if(url) {
+      return (
+        <a href={url} target='_blank' rel="noopener noreferrer"><img src='https://s3.amazonaws.com/my-politician-app.herokuapp.com/homepage.png' alt='homepage icon'/></a>
+      )};
+  }
+  const twitter = (userName) => {
+    if(userName) {
+      return (
+        <a href={`https://twitter.com/${userName}`} target='_blank' rel="noopener noreferrer"><img src='https://s3.amazonaws.com/my-politician-app.herokuapp.com/twitter.png' alt='twitter logo'/></a>
+      )};
+  }
+  const facebook = (userName) => {
+    if(userName) {
+      return (
+        <a href={`https://www.facebook.com/${userName}`} target='_blank' rel="noopener noreferrer"><img src='https://s3.amazonaws.com/my-politician-app.herokuapp.com/facebook.png' alt='facebook logo'/></a>
+      )};
+  }
+  const youtube = (userName) => {
+    if(userName) {
+      return (
+        <a href={`https://www.youtube.com/${userName}`} target='_blank' rel="noopener noreferrer"><img src='https://s3.amazonaws.com/my-politician-app.herokuapp.com/youtube.png' alt='youtube logo'/></a>
+      )};
+  }
   return (
-    <div>
-      <p>Website: {props.url}</p>      
-      <p>Facebook: {props.facebook}</p>
-      <p>Twitter: {props.twitter}</p>
-      <p>Youtube: {props.youtube}</p>
+    <div className='social'>
+      {homepage(props.url)}
+      {twitter(props.twitter)}
+      {facebook(props.facebook)}
+      {youtube(props.youtube)}
     </div>
   );
 }
