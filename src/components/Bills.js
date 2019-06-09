ef61aad6ca08cbdf177302f88009bd711e1eb8a9
NickHimmel <himmel.nick@gmail.com>
2019-06-09 16:26:41 -0400

    Add tables to bills

--- src/components/Bills.js
+++ src/components/Bills.js
@@ -3,29 +3,39 @@ const shortid = require('shortid');
 
 const Bills = (props) => {
   const bills = props.bills.map((bill) =>
-    <li key={shortid.generate()}>
-      <p>The {bill.congress} United States Congress</p>
-      <p>{bill.introduced_date}</p>
-      <p>{bill.committees}</p>
-      <h3>{bill.short_title}</h3>
-      <p>Primary Subject: {bill.primary_subject}</p>
-      <p>Bill Summary: {bill.summary_short}</p>
-      <p>#Cosponsors: {bill.cosponsors}</p>
-      <p>Cosponsors By Party: D:{bill.cosponsors_by_party.D} R:{bill.cosponsors_by_party.D}</p>
-      <p>Last Action: {bill.latest_major_action} on {bill.latest_major_action_date}</p>
-      <p>Last Vote: {bill.last_vote}</p>
-      <p>Bill Active: {bill.active}</p>
-      <p>Last Vote: {bill.last_vote}</p>
-      <p>Passed in the House: {bill.house_passage}</p>
-      <p>Passed in the Senate: {bill.senate_passage}</p>
-    </li>
+    <tr key={shortid.generate()}>
+      <td>The {bill.congress} United States Congress {bill.introduced_date}</td>
+      <td>{bill.short_title}</td>
+      <td>{bill.summary_short}</td>
+      <td>{bill.cosponsors}</td>
+      <td>D:{bill.cosponsors_by_party.D} R:{bill.cosponsors_by_party.R}</td>
+      <td>{bill.latest_major_action} on {bill.latest_major_action_date}</td>
+      <td>{bill.active}</td>
+      <td>{bill.last_vote}</td>
+      <td>{bill.house_passage}</td>
+      <td>{bill.senate_passage}</td>
+    </tr>
   );
   return (
     <div className="bills politician-info">
       <h3>Last Twenty Bills Sponsored</h3>
-      <ul>
-        {bills}
-      </ul>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>Title</th>
+            <th>Summary</th>
+            <th>Number of Cosponsors</th>
+            <th>Cosponsors By Party</th>
+            <th>Action</th>
+            <th>Active</th>
+            <th>Last Vote</th>
+            <th>Passed in the House</th>
+            <th>Passed in the Senate</th>
+          </tr>
+          {bills}
+        </tbody>
+      </table>
     </div>
   );
 }
