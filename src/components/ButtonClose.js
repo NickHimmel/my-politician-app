a0a789fed09ca8ddd08bf564a8a0e5ee5e7d520d
NickHimmel <himmel.nick@gmail.com>
2019-06-10 16:28:47 -0400

    Style close button

--- src/components/ButtonClose.js
+++ src/components/ButtonClose.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const ButtonClose = (props) => {
   return (
-    <button className="button" onClick={props.onClick}>
-      <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M14 1.4L12.6 0 7 5.6 1.4 0 0 1.4 5.6 7 0 12.6 1.4 14 7 8.4l5.6 5.6 1.4-1.4L8.4 7z" fill="#000" fillRule="evenodd"/></svg>
+    <button className="button-close" onClick={props.onClick}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 18 18">
+        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/>
+      </svg>
     </button>
   );
 }
