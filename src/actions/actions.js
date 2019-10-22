87f08a56439c68fd0ccd65cc1d46bcc969db7bbf
NickHimmel <himmel.nick@gmail.com>
2019-10-22 18:05:10 -0400

    test request header

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -121,9 +121,14 @@ export const fetchFinances = (cid) => {
   return (dispatch, getState) => {
     dispatch(startFetchFinances());
     const token = process.env.REACT_APP_OPEN_SECRETS_API_KEY;
+    const REQUEST_HEADER = {
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+      }
+    }
     if (cid) {
       Promise.all([
-        axios.get(`https://cors-anywhere.herokuapp.com/https://www.opensecrets.org/api/?method=candSummary&cid=${cid}&output=json&apikey=${token}`),
+        axios.get(`https://cors-anywhere.herokuapp.com/https://www.opensecrets.org/api/?method=candSummary&cid=${cid}&output=json&apikey=${token}`, REQUEST_HEADER),
         axios.get(`https://cors-anywhere.herokuapp.com/https://www.opensecrets.org/api/?method=candContrib&cid=${cid}&output=json&apikey=${token}`),
         axios.get(`https://cors-anywhere.herokuapp.com/https://www.opensecrets.org/api/?method=candIndustry&cid=${cid}&output=json&apikey=${token}`),
         axios.get(`https://cors-anywhere.herokuapp.com/https://www.opensecrets.org/api/?method=candSector&cid=${cid}&output=json&apikey=${token}`)
