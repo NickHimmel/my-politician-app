f42e8700f43bd14cf510d4c366f3bc8e4aef18ed
NickHimmel <himmel.nick@gmail.com>
2019-11-09 18:13:33 -0500

    add initial mobile styles

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -5,21 +5,22 @@ const NavHeader = (props) => {
     <div className='nav'>
       <ul className='nav-header'>
         <li>
-          <button onClick={(e) => props.onClick(e, props.house.id, props.house.next_election)}>
+          <button id='nav-header-active' className='nav-header-item' onClick={(e) => props.onClick(e, props.house.id, props.house.next_election)}>
             <span className='label'>Representative</span><p id='active-button'>{props.house.name} ({props.house.party})</p>
           </button>
         </li>
         <li>
-          <button onClick={(e) => props.onClick(e, props.senatorOne.id, props.senatorOne.next_election)}>
+          <button className='nav-header-item' onClick={(e) => props.onClick(e, props.senatorOne.id, props.senatorOne.next_election)}>
             <span className='label'>Senator</span><p>{props.senatorOne.name} ({props.senatorOne.party})</p>
           </button>
         </li>
         <li>
-          <button onClick={(e) => props.onClick(e, props.senatorTwo.id, props.senatorTwo.next_election)}>
+          <button className='nav-header-item' onClick={(e) => props.onClick(e, props.senatorTwo.id, props.senatorTwo.next_election)}>
             <span className='label'>Senator</span><p>{props.senatorTwo.name} ({props.senatorTwo.party})</p>
           </button>
         </li>
       </ul>
+      <button className='nav-mobile-button'>&#709;</button>
     </div>
   );
 }
